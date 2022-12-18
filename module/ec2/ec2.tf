4e09d437c784ecc917ab7e1d18d41f9e6619ef53
Makito Katsube <katsubemakito@gmail.com>
2022-12-19 03:00:22 +0900

    80番を開けた

--- module/ec2/ec2.tf
+++ module/ec2/ec2.tf
@@ -42,6 +42,12 @@ resource "aws_security_group" "webserver_sg" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
     egress {
         from_port = 0
         to_port = 0
